80a1063e76eb90ee03e9b7d066f9eba5a8382af0
pumanitro <patrykjanik1710@gmail.com>
2020-12-10 13:24:01 +0100

    Added dedicated console.log after form submission.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import React, { FunctionComponent } from 'react';
 import { NewEventHeader } from 'components/NewEventHeader/NewEventHeader';
 import { About } from './About/About';
@@ -24,7 +25,27 @@ export const HomeContent: FunctionComponent = () => (
       amOrPm: 'AM',
       duration: '',
     }}
-    onSubmit={formBag => console.log(formBag)}
+    onSubmit={formBag => {
+      const splitTime = formBag.values.time.split(':');
+      const time = `${formBag.values.amOrPm === 'AM' ? splitTime[0] : splitTime[0] + 12}:${splitTime[1]}`;
+
+      const responsiblePerson = JSON.parse(formBag.values.responsible);
+
+      console.log({
+        title: formBag.values.title,
+        description: formBag.values.description,
+        category_id: formBag.values.category,
+        paid_event: formBag.values.payment === 'Paid event',
+        event_fee: formBag.values.fee,
+        reward: formBag.values.reward,
+        date: `${formBag.values.date}T${time}`,
+        duration: formBag.values.duration * 3600,
+        coordinator: {
+          email: formBag.values.email,
+          id: responsiblePerson.id,
+        },
+      });
+    }}
   >
     <NewEventHeader />
 
