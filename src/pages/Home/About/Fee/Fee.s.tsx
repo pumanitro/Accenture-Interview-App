edd6907890c1c4af10289771c404f465e56d1b05
pumanitro <patrykjanik1710@gmail.com>
2020-12-11 17:24:21 +0100

    Added more margin to dollar field.

---
+++ src/pages/Home/About/Fee/Fee.s.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const StyledDollarWrapper = styled.span`
+  margin-left: 12px;
+`;
--- src/pages/Home/About/Fee/Fee.tsx
+++ src/pages/Home/About/Fee/Fee.tsx
@@ -2,6 +2,7 @@ import { useObjectField } from 'global/ObjectForm/useObjectField';
 import { FormInput } from 'components/FormInput/FormInput';
 import { isRequired } from 'helpers/formValidations';
 import React from 'react';
+import { StyledDollarWrapper } from './Fee.s';
 
 export const Fee = () => {
   const { value } = useObjectField('payment');
@@ -10,7 +11,7 @@ export const Fee = () => {
     return (
       <>
         <FormInput type="number" width="120px" min={0} placeholder="Fee" name="fee" validateFunction={isRequired} />
-        {'$'}
+        <StyledDollarWrapper>$</StyledDollarWrapper>
       </>
     );
   }
